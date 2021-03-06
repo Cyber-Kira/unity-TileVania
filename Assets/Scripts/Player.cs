e490dfb84bd7a5e460e2266d5453fd42eab6b9ea
Kirill Koloskov <kkoloskov02@gmail.com>
2021-03-06 18:38:15 +0300

    Player can run left and right

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D myRigidBody;
+
+    private void Start() {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+    private void Update() {
+        Run();
+    }
+
+    private void Run() {
+        float controlThrow = Input.GetAxis("Horizontal") * runSpeed;
+        Vector2 playerVelocity = new Vector2(controlThrow, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}
