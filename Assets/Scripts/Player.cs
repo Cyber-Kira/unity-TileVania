e50990f79ae124dec02de61511679b8a8ced4ba7
Kirill Koloskov <kkoloskov02@gmail.com>
2021-03-06 18:44:08 +0300

    Flip player when moving right and left

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,11 +12,20 @@ public class Player : MonoBehaviour
     }
     private void Update() {
         Run();
+        FlipSprite();
     }
 
     private void Run() {
         float controlThrow = Input.GetAxis("Horizontal") * runSpeed;
         Vector2 playerVelocity = new Vector2(controlThrow, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
