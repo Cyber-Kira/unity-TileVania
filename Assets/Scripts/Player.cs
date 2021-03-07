6ecd9f95f535b03aa1dfe37791ffa408652bd907
Kirill Koloskov <kkoloskov02@gmail.com>
2021-03-07 14:55:59 +0300

    Add jump and ladder

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     // Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 1f;
 
     // State
     bool isAlive = true;
@@ -13,13 +15,19 @@ public class Player : MonoBehaviour
     // Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
+    float gravityScaleAtStart;
 
     private void Start() {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
     }
     private void Update() {
         Run();
+        Jump();
+        ClimbLadder();
         FlipSprite();
     }
 
@@ -30,6 +38,33 @@ public class Player : MonoBehaviour
 
         myRigidBody.velocity = playerVelocity;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump() {
+
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            return;
+        }
+        if (Input.GetButtonDown("Jump")) {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void ClimbLadder() {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("Climbing", false);
+            return;
+        }
+
+        float controlThrow = Input.GetAxis("Vertical") * climbSpeed;
+        Vector2 climbingVelocityToAdd = new Vector2(myRigidBody.velocity.x, controlThrow);
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+        myRigidBody.velocity = climbingVelocityToAdd;
+        myRigidBody.gravityScale = 0f;
     }
 
     private void FlipSprite() {
